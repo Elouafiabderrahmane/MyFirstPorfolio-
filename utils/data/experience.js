a827a8a474492db443b10884278cfef6977cd02d
Elouafi Abderrahmane <abderrahmane.elouafi-etu@etu.univh2c.ma>
2024-09-30 20:30:51 +0100

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,14 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    title: 'Software Engineer ',
+    company: "BARAKATOUNA",
+    duration: "(Jan 2022 -september)"
   },
   {
     id: 2,
     title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
-  },
-  {
-    id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
+    company: "3D Smart factory ",
+    duration: "(Jun 2023 - Jan 2024)"
   }
 ]
